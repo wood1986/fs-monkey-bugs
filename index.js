95bad61782d52d1bdbc18cee504b43bc3f8fc797
wood1986 <5212215+wood1986@users.noreply.github.com>
2021-09-19 23:35:35 -0700

    WIP

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
-// const fs = require('fs');
-// const { ufs } = require('unionfs');
-// const { fs:memfs, vol } = require('memfs');
-// const { patchFs, patchRequire } = require('fs-monkey');
-
-// const ofs = { ...fs };
-
-// ufs
-//   .use(memfs)
-//   .use(ofs);
-// patchRequire(ufs);
-// patchFs(ufs)
-
-// console.log(fs, ofs)
-
-// fs.writeFileSync(`/memfs.log.js`, "module.exports.log = () => { console.log(`Hello from memfs.log.js`) }");
-// fs.writeFileSync(`${__dirname}/fs.log.js`, "module.exports.log = () => { console.log(`Hello from fs.log.js`) }")
-
-// require(`/memfs.log.js`).log();
-// require(`${__dirname}/fs.log.js`).log();
 
 const fs = require(`fs`);
 const {ufs} = require(`unionfs`);
-const {fs: memfs} = require(`memfs`);
+const {fs:memfs} = require(`memfs`);
 const {patchFs, patchRequire} = require(`fs-monkey`);
-
-const ofs = {...fs};
-
-memfs.writeFileSync(`/memfs.log.js`, `module.exports.log = () => { console.log(\`Hello from memfs.log.js\`) }`);
-fs.writeFileSync(`${__dirname}/fs.log.js`, `module.exports.log = () => { console.log(\`Hello from fs.log.js\`) }`);
 
 ufs
   .use(memfs)
-  .use(ofs);
+  .use(fs);
 patchFs(ufs); // make fs recognizes ufs
+patchRequire(ufs);
 
-patchRequire(fs);
+fs.writeFileSync(`./fs.log.js`, `module.exports.log = () => { console.log(\`Hello from fs.log.js\`) }`);
+console.log("fs", fs.readFileSync(`./fs.log.js`));
+console.log("ufs", ufs.readFileSync(`./fs.log.js`));
+require(`./fs.log.js`).log();
 
-// require(`pnpapi`).resetRealFs(fs);
-
-require(`${__dirname}/fs.log.js`).log();
-console.log(ufs.readFileSync(`/memfs.log.js`));
-console.log(memfs.readFileSync(`/memfs.log.js`));
-console.log(fs.readFileSync(`/memfs.log.js`));
-
+fs.writeFileSync(`/memfs.log.js`, `module.exports.log = () => { console.log(\`Hello from memfs.log.js\`) }`);
+console.log("fs", fs.readFileSync(`/memfs.log.js`));
+console.log("ufs", ufs.readFileSync(`/memfs.log.js`));
+console.log("memfs", memfs.readFileSync(`/memfs.log.js`));
 require(`/memfs.log.js`).log();
+
+
+
