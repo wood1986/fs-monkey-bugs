ee32a20f28f8125dae18496c8e1f875adb5e823f
wood1986 <5212215+wood1986@users.noreply.github.com>
2021-09-20 10:07:15 -0700

    WIP

--- index.js
+++ index.js
@@ -2,13 +2,23 @@
 const fs = require(`fs`);
 const {ufs} = require(`unionfs`);
 const {fs:memfs} = require(`memfs`);
-const {patchFs, patchRequire} = require(`fs-monkey`);
 
-ufs
-  .use(memfs)
-  .use({...fs});
-patchFs(ufs); // make fs recognizes ufs
-patchRequire(ufs);
+if (process.versions.pnp) {
+  const {patchNodeFs} = require("pnpapi")
+  patchNodeFs((originalFs) => {
+    ufs
+      .use(memfs)
+      .use(originalFs)
+    return ufs
+  })
+} else {
+  const {patchFs, patchRequire} = require(`fs-monkey`);
+    ufs
+    .use(memfs)
+    .use({...fs});
+  patchFs(ufs);
+  patchRequire(ufs);
+}
 
 fs.writeFileSync(`./fs.log.js`, `module.exports.log = () => { console.log(\`Hello from fs.log.js\`) }`);
 console.log("fs", fs.readFileSync(`./fs.log.js`));
@@ -20,6 +30,3 @@ console.log("fs", fs.readFileSync(`/memfs.log.js`));
 console.log("ufs", ufs.readFileSync(`/memfs.log.js`));
 console.log("memfs", memfs.readFileSync(`/memfs.log.js`));
 require(`/memfs.log.js`).log();
-
-
-
