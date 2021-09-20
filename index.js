b07b18eafd426ed043f72d52899062f27debc744
wood1986 <5212215+wood1986@users.noreply.github.com>
2021-09-20 11:10:02 -0700

    WIP

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ if (process.versions.pnp && require("pnpapi").patchNodeFs) {
 }
 
 fs.writeFileSync(`./fs.log.js`, `module.exports.log = () => { console.log(\`Hello from fs.log.js\`) }`);
-console.log("fs", fs.readFileSync(`./fs.log.js`));
-console.log("ufs", ufs.readFileSync(`./fs.log.js`));
+console.log("fs", fs.readFileSync(`./fs.log.js`).toString());
+console.log("ufs", ufs.readFileSync(`./fs.log.js`).toString());
 require(`./fs.log.js`).log();
 
 fs.writeFileSync(`/memfs.log.js`, `module.exports.log = () => { console.log(\`Hello from memfs.log.js\`) }`);
-console.log("fs", fs.readFileSync(`/memfs.log.js`));
-console.log("ufs", ufs.readFileSync(`/memfs.log.js`));
-console.log("memfs", memfs.readFileSync(`/memfs.log.js`));
+console.log("fs", fs.readFileSync(`/memfs.log.js`).toString());
+console.log("ufs", ufs.readFileSync(`/memfs.log.js`).toString());
+console.log("memfs", memfs.readFileSync(`/memfs.log.js`).toString());
 require(`/memfs.log.js`).log();
